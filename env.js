bae34edac6edfb15c6419213dc1ab047618e272e
LuisPalomino91 <luis.dom.palomino91@gmail.com>
2025-02-20 20:42:29 -0600

    Se sube modificacion del logo

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-export const API_SERVICEID = import.meta.env.API_SERVICEID;
-export const API_PUBLICKEY = import.meta.env.API_PUBLICKEY;
-export const API_TEMPLATE_CONTAC = import.meta.env.API_TEMPLATE_CONTAC;
-export const API_TEMPLATE_PROMO = import.meta.env.API_TEMPLATE_PROMO;
+export const API_SERVICEID = import.meta.env.VITE_API_SERVICEID;
+export const API_PUBLICKEY = import.meta.env.VITE_API_PUBLICKEY;
+export const API_TEMPLATE_CONTAC = import.meta.env.VITE_API_TEMPLATE_CONTAC;
+export const API_TEMPLATE_PROMO = import.meta.env.VITE_API_TEMPLATE_PROMO;
